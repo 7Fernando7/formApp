1918e54fb9392096a7fbada77fa981606288e750
Pereira De Paiva <fernando.pereira@transfesa.com>
2023-11-20 11:10:47 +0100

    Elaborado el validador de correo

--- src/app/shared/validators/email-validator.service.ts
+++ src/app/shared/validators/email-validator.service.ts
@@ -2,7 +2,7 @@
 
 import { Injectable } from '@angular/core';
 import { AbstractControl, AsyncValidator, ValidationErrors } from '@angular/forms';
-import { Observable, delay, of } from 'rxjs';
+import { Observable, Subscriber, delay, map, of } from 'rxjs';
 
 @Injectable({providedIn: 'root'})
 export class EmailValidatorService implements AsyncValidator {
@@ -12,12 +12,56 @@ export class EmailValidatorService implements AsyncValidator {
     const email = control.value;
     console.log({ email });
 
-    return of({
-      emailTaken: true,
+    const httpCallObservable =  new Observable<ValidationErrors | null>(( subscriber ) =>  {
+
+      console.log({ email });
+
+      if( email === 'fernando@google.com') {
+        subscriber.next( { emailTaken: true });
+        subscriber.complete();
+        //return;
+      }
+
+      subscriber.next( null );
+      subscriber.complete();
+
     }).pipe(
-      delay(2000)
+      delay(3000)
     );
+
+    return httpCallObservable;
+
+
   }
+
+
+
+
+
+  // validate(control: AbstractControl ): Observable<ValidationErrors | null> {
+
+  //   const email = control.value;
+  //   console.log({ email });
+
+  //   return of({
+  //     emailTaken: true,
+  //   }).pipe(
+  //     delay(2000)
+  //   );
+  // }
+
+
+  // return this.http.get<any[]>(`http://localhost:3000/users?q=${ email }`)
+  // .pipe(
+  //   delay(3000)
+
+  //   map( resp => {
+  //     return ( resp.length === 0)
+
+  //   ? null
+  //   : { emailTaken: true}
+  //   })
+  // );
 
 
 
